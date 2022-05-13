9c378e6330c91249f7bca3dfad7ba4b809f05a5d
bogusmaciej <s223136@student.uek.krakow.pl>
2022-05-13 11:39:08 +0200

    MB 2022-05-13

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 print("\nLista dostępnych opinii:")
 print(*[filename.split(".")[0] for filename in os.listdir("./opinions")], sep="\n")
@@ -8,3 +9,31 @@ product_id = input("\nPodaj id produktu: ")
 
 opinions = pd.read_json(f"opinions/{product_id}.json")
 print(opinions)
+
+
+opinions.stars = opinions.stars.map(lambda x: float(x.split("/")[0].replace(",", ".")))
+opinions_count = len(opinions.index)
+pros_count = opinions.pros.map(bool).sum()
+cons_count = opinions.cons.map(bool).sum()
+avarage_score = opinions.stars.mean().round(2)
+
+recommendation = opinions.recomendation.value_counts(dropna = False)
+recommendation.plot.pie(
+    label="", 
+    autopct="%1.1f%%", 
+    colors=["green", "blue", "red"]
+)
+plt.title("Rekomendacja")
+plt.savefig(f"plots/{product_id}_recomendations.png")
+plt.close()
+
+stars = opinions.stars.value_counts()
+stars.plot.bar()
+plt.title("Ocenty produktów")
+plt.xlabel("Liczba gwiazdek")
+plt.ylabel("Liczba opinii")
+plt.grid(True)
+plt.xticks(rotation=0)
+plt.show()
+
+print(recommendation)
